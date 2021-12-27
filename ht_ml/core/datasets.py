8e575004d99a25156a4a8a6082583536c3a23044
codera21 <atul.nepal@gmail.com>
2021-12-27 12:04:15 +0545

    new  commit

--- ht_ml/core/datasets.py
+++ ht_ml/core/datasets.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+import os
 
-class Datasets: 
+class Datasets:
     def __init__(self, dataset_name) -> None:
         self.dataset_name = dataset_name.strip().lower()
-        self.dataset_file =  f'../../data/{dataset_name}/datasets'
-    
-    
-    def read_dataset(self, n= 1000):
+        self.dataset_path = f"{str(Path(__file__).parent.parent.parent)}/data/{self.dataset_name}/datasets/default/"
+
+    def read_dataset(self, n=1000, max=None):
         # get first n files from the dataset and return them as a list
-        for i in range(n):
-            with open(self.dataset_file, 'r') as f:
-                pass
+        print(os.walk(self.dataset_path))
+        print(self.dataset_path)
+        # for i in range(n):
+        #     with open(self.dataset_file, 'r') as f:
+        #         pass
